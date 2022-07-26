5a5e69f38cb4190a82229654db58faadb2eff144
Danilo Costa <63356168+daniloalvescosta@users.noreply.github.com>
2022-07-26 11:44:37 -0300

    Revert "bug fix for heroku"
    This reverts commit 9fee99388d14e5d02d04672d3c6a4086af9c0d64.

--- app/services/GitHubApi.rb
+++ app/services/GitHubApi.rb
@@ -12,7 +12,7 @@ class GitHubApi
     @langs = ['ruby', 'java', 'python', 'c++', 'rust']
 
     if @langs.include?(languages)
-    @options[:query] = { per_page: 20, q: languages.to_s, sort: 'stars', order: 'desc'}
+    @options[:query] = { per_page: 20, q: languages.to_s, sort: 'stars', order: 'desc', authorization: ENV['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'] }
     self.class.get('/search/repositories', @options)
     else
       false
@@ -20,6 +20,7 @@ class GitHubApi
   end
 
   def get_repository(owner, repository_name)
-    self.class.get("/repos/#{owner}/#{repository_name}")
+    @options[:query] = { authorization: ENV['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'] }
+    self.class.get("/repos/#{owner}/#{repository_name}", @options)
   end
 end
