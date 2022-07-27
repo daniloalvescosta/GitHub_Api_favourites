1b5e397be93ba8b0721026a4cf2a3479792302e6
Danilo Costa <63356168+daniloalvescosta@users.noreply.github.com>
2022-07-26 22:10:56 -0300

    Altera home controller
    Altera a Home controller para que os parametros batam com os informados nas rotas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,20 +3,13 @@
 class HomeController < ApplicationController
   before_action :set_global_github_api
 
-  def index; end
+  def index
+    @repository = @github_api.get_repository(params[:owner], params[:repository_name]) 
+  end
 
   def search
     @languages = params[:languages].downcase
     @repositories = @github_api.get_repositories(@languages)
   end
 
-  def show
-    @repository = @github_api.get_repository(params[:owner], params[:repository_name])
-  end
-
-  private
-
-  def set_global_github_api
-    @github_api = GitHubApi.new
-  end
 end
