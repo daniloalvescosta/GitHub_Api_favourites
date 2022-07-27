9d4dad7df8e812a8ade947e7cfecc1e20c8f712b
Danilo Costa <63356168+daniloalvescosta@users.noreply.github.com>
2022-07-26 22:05:32 -0300

    adiciona metodo global
    Adiciona o método "set_global_github_api" no Application controller para ser utilizado por todos os outros controllers e assim evitar repetição de código

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    before_action :set_global_github_api
+
+    def set_global_github_api
+        @github_api ||= GitHubApi.new
+    end
 end
